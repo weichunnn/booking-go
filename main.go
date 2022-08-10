3563fa32e11dc7c272d8f7817f1e28f980912a40
Wei Chun <tanweichun@outlook.com>
2022-08-10 23:36:12 +0800

    hello world

---
+++ main.go
@@ -0,0 +1,10 @@
+// main package of program
+// all code must be in a package
+package main
+
+import "fmt"
+
+// to let go know where to start executing
+func main() {
+	fmt.Println("Hello World")
+}
